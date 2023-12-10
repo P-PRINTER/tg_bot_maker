c03a5ca17a1bae380e8f547423230220fad6dcb5
P-PRINTER <psevdoprinter@gmail.com>
2023-12-11 00:52:42 +0200

    fixed utils.check_data_for_primitiveness work and entered utils.build_tg_object func instead a lot of others

--- utils.py
+++ utils.py
@@ -17,18 +17,21 @@ tg_handlers: dict[str, Callable] = {
 	'inline_keyboard_button'			: InlineKeyboardButton,
 }
 
+system_names_switch = {
+	'handler_type'
+}
+
 def check_data_for_primitiveness (data: Any) -> bool:
 
 	result = False
 
 	switch = {
-		type(int),
-		type(float),
-		type(complex),
-		type(str),
-		type(bool),
+		type(0),
+		type(0.0),
+		type(0j),
+		type(""),
+		type(True),
 	}
-
 	if type(data) in switch:
 		result = True
 
@@ -42,18 +45,20 @@ def build_tg_object (config: dict[str, Any]) -> Any:
 	handler_type: str 		= config['handler_type']
 	handler		: Callable 	= tg_handlers[handler_type]
 
-	for arg_name, arg_content in config.items:
-		
+	for arg_name, arg_content in config.items():
+		if arg_name in system_names_switch:
+			continue
+
 		if not check_data_for_primitiveness(arg_content):
 			arg_content = build_tg_object(arg_content)
 
 		arg_dict[arg_name] = arg_content
 
-	result = handler(extra_data = arg_dict)
 
+	result = handler(**arg_dict)
 	return result
 
-def build_optional_arg (arg_name: str, config: dict, handler_func: Optional[Callable] = None) -> Any:
+def build_arg (arg_name: str, config: dict) -> Any:
 
 	result_arg: Any = None
 
@@ -73,56 +78,56 @@ def build_optional_arg (arg_name: str, config: dict, handler_func: Optional[Call
 	return result_arg
 
 
-def web_app_builder (config: dict[str, Any]) -> WebAppInfo:
+# def web_app_builder (config: dict[str, Any]) -> WebAppInfo:
 
-	# WebAppInfo arguments
-	url: str = config['url']
+# 	# WebAppInfo arguments
+# 	url: str = config['url']
 
-	return WebAppInfo(url = url)
+# 	return WebAppInfo(url = url)
 
-def login_url_builder (config: dict[str, Any]) -> LoginUrl:
+# def login_url_builder (config: dict[str, Any]) -> LoginUrl:
 
-	# LoginUrl arguments
-	url: 					str 			= config['url']
-	forward_text: 			Optional[str]	= build_optional_arg('forward_text', config)
-	bot_username:			Optional[str] 	= build_optional_arg('bot_username', config)
-	request_write_access:	Optional[bool] 	= build_optional_arg('request_write_access', config)
+# 	# LoginUrl arguments
+# 	url: 					str 			= config['url']
+# 	forward_text: 			Optional[str]	= build_arg('forward_text', config)
+# 	bot_username:			Optional[str] 	= build_arg('bot_username', config)
+# 	request_write_access:	Optional[bool] 	= build_arg('request_write_access', config)
 
-	return LoginUrl(
-		url 					= url,
-		forward_text 			= forward_text,
-		bot_username 			= bot_username,
-		request_write_access 	= request_write_access
-	)
+# 	return LoginUrl(
+# 		url 					= url,
+# 		forward_text 			= forward_text,
+# 		bot_username 			= bot_username,
+# 		request_write_access 	= request_write_access
+# 	)
 
-def switch_inline_query_chosen_chat_builder (config: dict[str, Any]) -> SwitchInlineQueryChosenChat:
+# def switch_inline_query_chosen_chat_builder (config: dict[str, Any]) -> SwitchInlineQueryChosenChat:
 
-	# SwitchInlineQueryChosenChat arguments
-	query: 					Optional[str]	= build_optional_arg('query', config)
-	allow_user_chats: 		Optional[bool]	= build_optional_arg('allow_user_chats', config)
-	allow_bot_chats: 		Optional[bool]	= build_optional_arg('allow_bot_chats', config)
-	allow_group_chats: 		Optional[bool]	= build_optional_arg('allow_group_chats', config)
-	allow_channel_chats: 	Optional[bool]	= build_optional_arg('allow_channel_chats', config)
+# 	# SwitchInlineQueryChosenChat arguments
+# 	query: 					Optional[str]	= build_arg('query', config)
+# 	allow_user_chats: 		Optional[bool]	= build_arg('allow_user_chats', config)
+# 	allow_bot_chats: 		Optional[bool]	= build_arg('allow_bot_chats', config)
+# 	allow_group_chats: 		Optional[bool]	= build_arg('allow_group_chats', config)
+# 	allow_channel_chats: 	Optional[bool]	= build_arg('allow_channel_chats', config)
 
-	return SwitchInlineQueryChosenChat(
-		query					= query,
-		allow_user_chats		= allow_user_chats,
-		allow_bot_chats			= allow_bot_chats,
-		allow_group_chats		= allow_group_chats,
-		allow_channel_chats		= allow_channel_chats
-	)
+# 	return SwitchInlineQueryChosenChat(
+# 		query					= query,
+# 		allow_user_chats		= allow_user_chats,
+# 		allow_bot_chats			= allow_bot_chats,
+# 		allow_group_chats		= allow_group_chats,
+# 		allow_channel_chats		= allow_channel_chats
+# 	)
 
-def callback_game_builder (config: dict[str, Any]) -> CallbackGame:
+# def callback_game_builder (config: dict[str, Any]) -> CallbackGame:
 
-	return CallbackGame()
+# 	return CallbackGame()
 
 
-def build_btn_arg (arg_name, btn_config) -> Any:
+# def build_btn_arg (arg_name, btn_config) -> Any:
 
-	if not(arg_name in btn_config):
-		return None
+# 	if not(arg_name in btn_config):
+# 		return None
 
-	return btn_config[arg_name]
+# 	return btn_config[arg_name]
 
 
 
@@ -131,60 +136,63 @@ def build_inline_btn (btn_name: str) -> InlineKeyboardButton:
 	BTNS: dict 			= keyboards_config['inline_buttons']
 	btn_config: dict 	= BTNS[btn_name]
 
-	text:								str 			= btn_config['text']
-	url: 								Optional[
-		str] 								= build_optional_arg('url', btn_config)
-	callback_data: 						Optional[
-		str] 								= build_optional_arg('callback_data', btn_config)
-	web_app: 							Optional[
-		types.WebAppInfo] 					= build_optional_arg(
-			'web_app',
-			btn_config,
-			web_app_builder
-		)
-	login_url: 							Optional[
-		types.LoginUrl]						= build_optional_arg(
-			'login_url',
-			btn_config,
-			login_url_builder
-		)
-	switch_inline_query: 				Optional[
-		str] 								= build_optional_arg(
-			'switch_inline_query',
-			btn_config
-		)
-	switch_inline_query_current_chat: 	Optional[
-		str] 								= build_optional_arg(
-			'switch_inline_query_current_chat',
-			btn_config
-		)
-	switch_inline_query_chosen_chat: 	Optional[
-		types.SwitchInlineQueryChosenChat]	= build_optional_arg(
-			'switch_inline_query_chosen_chat',
-			btn_config,
-			switch_inline_query_chosen_chat_builder
-		)
-	callback_game: 						Optional[
-		types.CallbackGame] = build_optional_arg(
-			'callback_game',
-			btn_config,
-			callback_game_builder
-		)
-	pay: 								Optional[
-		bool]								= build_optional_arg('pay', btn_config)
+	result_btn: InlineKeyboardButton = build_tg_object(btn_config)
+	return result_btn
+
+	# text:								str = build_arg('text', btn_config)
+	# url: 								Optional[
+	# 	str] 								= build_arg('url', btn_config)
+	# callback_data: 						Optional[
+	# 	str] 								= build_arg('callback_data', btn_config)
+	# web_app: 							Optional[
+	# 	types.WebAppInfo] 					= build_arg(
+	# 		'web_app',
+	# 		btn_config,
+	# 		web_app_builder
+	# 	)
+	# login_url: 							Optional[
+	# 	types.LoginUrl]						= build_arg(
+	# 		'login_url',
+	# 		btn_config,
+	# 		login_url_builder
+	# 	)
+	# switch_inline_query: 				Optional[
+	# 	str] 								= build_arg(
+	# 		'switch_inline_query',
+	# 		btn_config
+	# 	)
+	# switch_inline_query_current_chat: 	Optional[
+	# 	str] 								= build_arg(
+	# 		'switch_inline_query_current_chat',
+	# 		btn_config
+	# 	)
+	# switch_inline_query_chosen_chat: 	Optional[
+	# 	types.SwitchInlineQueryChosenChat]	= build_arg(
+	# 		'switch_inline_query_chosen_chat',
+	# 		btn_config,
+	# 		switch_inline_query_chosen_chat_builder
+	# 	)
+	# callback_game: 						Optional[
+	# 	types.CallbackGame] = build_arg(
+	# 		'callback_game',
+	# 		btn_config,
+	# 		callback_game_builder
+	# 	)
+	# pay: 								Optional[
+	# 	bool]								= build_arg('pay', btn_config)
 	
-	return InlineKeyboardButton(
-		text 								= text,
-		url 								= url,
-		callback_data 						= callback_data,
-		web_app								= web_app,
-		login_url 							= login_url,
-		switch_inline_query 				= switch_inline_query,
-		switch_inline_query_current_chat 	= switch_inline_query_current_chat,
-		switch_inline_query_chosen_chat 	= switch_inline_query_chosen_chat,
-		callback_game 						= callback_game,
-		pay 								= pay
-	)
+	# return InlineKeyboardButton(
+	# 	text 								= text,
+	# 	url 								= url,
+	# 	callback_data 						= callback_data,
+	# 	web_app								= web_app,
+	# 	login_url 							= login_url,
+	# 	switch_inline_query 				= switch_inline_query,
+	# 	switch_inline_query_current_chat 	= switch_inline_query_current_chat,
+	# 	switch_inline_query_chosen_chat 	= switch_inline_query_chosen_chat,
+	# 	callback_game 						= callback_game,
+	# 	pay 								= pay
+	# )
 
 
 def build_reply_markup (markup: [[str]]) -> list[ list[KeyboardButton] ]:
